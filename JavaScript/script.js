8b4aa383e7946cfe4588f60eaeab67efb9692611
Daviixd <davicruz1090@gmail.com>
2023-11-06 18:39:35 -0300

    Unikids

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,16 +5,3 @@ hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
-const form =document.getElementById('form');
-const campos =document.querySelectorAll('.required');
-const spans =document.querySelectorAll('.span-required')
-const emailRegex =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-form.addEventListener('submit', (event)=>{
- event.preventDefault();
-    nameValidate();
-    validarTelefone();
-    emailValidate();
-    mainPasswordValidate();
-    comparePassword();
-});
