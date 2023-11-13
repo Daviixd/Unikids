bf7c3614625515caf75edac6d1694366a0e5f137
Daviixd <davicruz1090@gmail.comgit config --global user.email davicruz1090@gmail.com>
2023-11-13 10:37:32 -0300

    UniKids

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,7 @@
-const hamburguer = document.querySelector(".mobileMenu");
-const navMenu = document.querySelector(".nav-menu");
+const hamburguer = document.querySelector('.mobileMenu')
+const navMenu = document.querySelector('.nav-menu')
 
-hamburguer.addEventListener("click", () => {
-    hamburguer.classList.toggle('active');
-    navMenu.classList.toggle('active');
+hamburguer.addEventListener('click', () => {
+    hamburguer.classList.toggle('active')
+    navMenu.classList.toggle('active')
 })
