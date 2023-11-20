0cbff5ca31705d0a95ec66228848f4fe47960cc4
Daviixd <davicruz1090@gmail.comgit config --global user.email davicruz1090@gmail.com>
2023-11-20 15:48:57 -0300

    UniKids

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,18 @@
-const menu = document.querySelector('.mobileMenu')
-const navMenu = document.querySelector('.nav-menu')
+const menu = document.querySelector(".mobileMenu");
+const navMenu = document.querySelector(".nav-menu");
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('active')
-    navMenu.classList.toggle('active')
-})
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+const btn = document.querySelector("#buttonFooter");
+const devs = document.querySelector(".devs");
+
+btn.addEventListener("click", function () {
+  if (devs.style.display === "none") {
+    devs.style.display = "block";
+  } else {
+    devs.style.display = "none";
+  }
+});
