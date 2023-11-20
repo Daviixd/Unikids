dfdb9d8ee0885ea982c1c6fc8ffca8e5e1c889d7
Daviixd <davicruz1090@gmail.comgit config --global user.email davicruz1090@gmail.com>
2023-11-20 08:56:23 -0300

    UniKids

---
+++ JavaScript/form.js
@@ -0,0 +1,62 @@
+const form = document.getElementById("form");
+const campos = document.querySelectorAll(".required");
+const spans = document.querySelectorAll(".span-required");
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  nameValidate();
+  validarTelefone();
+  emailValidate();
+  mainPasswordValidate();
+  comparePassword();
+});
+
+function setError(index) {
+  campos[index].style.border = "2px solid #e63636";
+  spans[index].style.display = "flex";
+}
+function removeError(index) {
+  campos[index].style.border = "";
+  spans[index].style.display = "none";
+}
+
+function nameValidate() {
+  if (campos[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
+}
+function validarTelefone() {
+  var regex = /^\d{11}$/;
+
+  if (regex.test(campos[1].value)) {
+    removeError(1);
+  } else {
+    setError(1);
+  }
+}
+
+function emailValidate() {
+  if (!emailRegex.test(campos[2].value)) {
+    setError(2);
+  } else {
+    removeError(2);
+  }
+}
+function mainPasswordValidate() {
+  if (campos[3].value.length <= 8) {
+    setError(3);
+  } else {
+    removeError(3);
+    comparePassword();
+  }
+}
+function comparePassword() {
+  if (campos[3].value === campos[4].value && campos[4].value.length >= 8) {
+    removeError(4);
+  } else {
+    setError(4);
+  }
+}
